06530d4ad8922597fc79f1431dc9156e92d4b068
memoryleak47 <memoryleak47@protonmail.ch>
2023-07-18 23:03:03 +0200

    impl rendering

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import matplotlib.pyplot as plt
+
 # [(Key, Priority)]
-DATA = [(10, 10), (20, 20), (30, 30)]
+DATA = [(10, 10), (20, 20), (30, 30), (5, 70), (300, 2), (202, 24), (201, 23)]
 
 # node = {"tuple": (_, _), "children": [_, _], "color"}
 
@@ -16,10 +18,42 @@ def create_treap(data):
 
 # annotates a treap with colors to express k-bunches
 def treap_to_ktreap(k, treap):
-    pass
+    return treap # TODO
+
+def treap_depth(treap):
+    if treap == None:
+        return 0
+
+    l = treap_depth(treap["children"][0])
+    r = treap_depth(treap["children"][1])
+    return max(l, r) + 1
+
+def render_ktreap_rec(ktreap, x, depth, max_depth, parent_pos, ax):
+    if ktreap == None:
+        return
+
+    color = "black"
+    if "color" in ktreap:
+        color = ktreap["color"]
+
+    ax.scatter([x], [-depth], color=color)
+    ax.text(x, -depth, str(ktreap["tuple"]), color=color)
+
+    if parent_pos:
+        plt.arrow(parent_pos[0], parent_pos[1], x-parent_pos[0], -depth-parent_pos[1])
+
+    delta = 2 ** (max_depth - depth)
+    render_ktreap_rec(ktreap["children"][0], x-delta, depth+1, max_depth, [x, -depth], ax)
+    render_ktreap_rec(ktreap["children"][1], x+delta, depth+1, max_depth, [x, -depth], ax)
 
 def render_ktreap(ktreap):
-    pass
+    fig, ax = plt.subplots(1, 1, figsize=(6,6))
+    ax.set_axis_off()
+
+    max_depth = treap_depth(ktreap)
+    render_ktreap_rec(ktreap, 0, 0, max_depth, None, ax)
+
+    plt.show()
 
 def render(k, data):
     treap = create_treap(data)

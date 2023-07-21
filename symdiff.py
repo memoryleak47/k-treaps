89ac73dd7acda6258298e9541ebc9f2195cffb52
memoryleak47 <memoryleak47@protonmail.ch>
2023-07-21 12:48:50 +0200

    improve symdiff

--- symdiff.py
+++ symdiff.py
@@ -2,15 +2,23 @@
 
 from ktreap import *
 
-MAX = 10000
-L = np.arange(MAX); random.shuffle(L)
-R = np.arange(MAX)
-data = list(np.stack([L, R]).T)
+def val(MAX):
+    L = np.arange(MAX); random.shuffle(L)
+    R = np.arange(MAX)
+    data = list(np.stack([L, R]).T)
 
-bunches1 = bunches(3, data)
+    bunches1 = bunches(3, data)
 
-data.pop() # removes highest prio element.
-bunches2 = bunches(3, data)
+    data.pop() # removes highest prio element.
+    bunches2 = bunches(3, data)
 
-print(len(bunches1), len(bunches2), len(bunches1 ^ bunches2))
+    return (len(bunches1), len(bunches2), len(bunches1 ^ bunches2))
+
+ITERS = 100
+for x in [10, 100, 1000, 10000]:
+    s = 0
+    for _ in range(ITERS):
+        s += val(x)[2]
+    s /= ITERS
+    print("{} -> {}".format(x, int(s)))
 
